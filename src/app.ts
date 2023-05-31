b0fe94b202b50889c6f075fd0b6fc4678f5774f8
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-05-31 13:37:05 +0300

    Updated entry file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import path from "path";
-// eslint-disable-next-line import/newline-after-import
+import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config({ path: path.join(__dirname, ".env") });
 // eslint-disable-next-line import/first
 import notesRoutes from "./routes/notes";
-// import cors from "cors";
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ credentials: true, origin: `${process.env.CLIENT}` }));
+
 app.use(notesRoutes);
 
 const port = process.env.PORT || 3006;
