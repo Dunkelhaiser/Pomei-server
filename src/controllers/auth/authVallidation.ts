d0876ae2418779ad33ba71289670060f24abe851
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-06-03 17:58:24 +0300

    Added login verification

--- src/controllers/auth/authVallidation.ts
+++ src/controllers/auth/authVallidation.ts
@@ -5,11 +5,13 @@ const schema = zod
     .object({
         username: zod
             .string()
+            .nonempty({ message: "Enter your username" })
             .min(6, { message: "Username must be at least 6 characters long" })
             .max(20, { message: "Username must be at maximum 20 characters long" }),
-        email: zod.string().email(),
+        email: zod.string().nonempty({ message: "Enter your email" }).email(),
         password: zod
             .string()
+            .nonempty({ message: "Enter your password" })
             .min(6, { message: "Password must be at least 6 characters long" })
             .max(36, { message: "Password must be at maximum 36 characters long" }),
         confirmPassword: zod.string().nonempty({ message: "Confirm your password" }),
@@ -23,6 +25,23 @@ export const validateSignUp = async (req: Request, res: Response, next: NextFunc
     const formData = req.body;
     try {
         await schema.parseAsync(formData);
+        next();
+    } catch (err) {
+        res.status(400).json({
+            status: (err as zod.ZodError).issues[0].message,
+        });
+    }
+};
+
+const schemaSignIn = zod.object({
+    login: zod.string().nonempty({ message: "Enter your username or email" }),
+    password: zod.string().nonempty({ message: "Enter your password" }),
+});
+
+export const validateSignIn = async (req: Request, res: Response, next: NextFunction) => {
+    const formData = req.body;
+    try {
+        await schemaSignIn.parseAsync(formData);
         next();
     } catch (err) {
         res.status(400).json({
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { signUp, checkAvailableData, signIn } from "../controllers/auth/authContoller";
-import { validateSignUp } from "../controllers/auth/authVallidation";
+import { validateSignIn, validateSignUp } from "../controllers/auth/authVallidation";
 
 const router = Router();
 
 router.post("/sign_up", validateSignUp, signUp);
 router.post("/sign_up_check", checkAvailableData);
-router.post("/sign_in", signIn);
+router.post("/sign_in", validateSignIn, signIn);
 
 export default router;
