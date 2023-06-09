bcad4f4feb64c6d9bf3f0fd5b6536ef8061841ad
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-06-09 17:37:13 +0300

    Added CRUD for notes

--- src/controllers/notes/notesController.ts
+++ src/controllers/notes/notesController.ts
@@ -20,3 +20,170 @@ export const loadNotes = async (req: AuthRequest, res: Response) => {
         });
     }
 };
+
+export const createNote = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user;
+        const { title, content } = req.body;
+        const result = await db.note.create({
+            data: {
+                title,
+                content,
+                user: {
+                    connect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+        res.status(201).json({
+            status: "success",
+            note: result,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "error",
+        });
+    }
+};
+
+export const updateNote = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user;
+        const noteId = req.params.id;
+        const { title, content } = req.body;
+
+        const note = await db.note.findFirst({
+            where: {
+                AND: [{ id: noteId }, { userId }],
+            },
+        });
+
+        if (!note) {
+            res.status(404).json({
+                status: "not found",
+            });
+            return;
+        }
+
+        const result = await db.note.update({
+            where: {
+                id: noteId,
+            },
+            data: {
+                title,
+                content,
+            },
+        });
+        res.status(200).json({
+            status: "success",
+            note: result,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "error",
+        });
+    }
+};
+
+export const deleteNote = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user;
+        const noteId = req.params.id;
+
+        const note = await db.note.findFirst({
+            where: {
+                AND: [{ id: noteId }, { userId }],
+            },
+        });
+
+        if (!note) {
+            res.status(404).json({
+                status: "not found",
+            });
+            return;
+        }
+
+        await db.note.delete({
+            where: {
+                id: noteId,
+            },
+        });
+        res.status(200).json({
+            status: "success",
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "error",
+        });
+    }
+};
+
+export const duplicateNote = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user;
+        const noteId = req.params.id;
+
+        const note = await db.note.findFirst({
+            where: {
+                AND: [{ id: noteId }, { userId }],
+            },
+        });
+
+        if (!note) {
+            res.status(404).json({
+                status: "not found",
+            });
+            return;
+        }
+
+        const result = await db.note.create({
+            data: {
+                title: note.title,
+                content: note.content,
+                user: {
+                    connect: {
+                        id: userId,
+                    },
+                },
+            },
+        });
+        res.status(201).json({
+            status: "success",
+            note: result,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "error",
+        });
+    }
+};
+
+export const loadNote = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user;
+        const noteId = req.params.id;
+
+        const note = await db.note.findFirst({
+            where: {
+                AND: [{ id: noteId }, { userId }],
+            },
+        });
+
+        if (!note) {
+            res.status(404).json({
+                status: "not found",
+            });
+            return;
+        }
+
+        res.status(200).json({
+            status: "success",
+            note,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "error",
+        });
+    }
+};
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { isAuthorized } from "../controllers/auth/authVallidation";
-import { loadNotes } from "../controllers/notes/notesController";
+import { createNote, deleteNote, duplicateNote, loadNote, loadNotes, updateNote } from "../controllers/notes/notesController";
 
 const router = Router();
 
 router.get("/", isAuthorized, loadNotes);
+router.post("/", isAuthorized, createNote);
+router.get("/:id", isAuthorized, loadNote);
+router.put("/:id", isAuthorized, updateNote);
+router.delete("/:id", isAuthorized, deleteNote);
+router.post("/duplicate/:id", isAuthorized, duplicateNote);
 
 export default router;
