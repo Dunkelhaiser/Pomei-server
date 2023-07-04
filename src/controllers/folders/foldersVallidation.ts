de841e85151360f7bf035ec0c2e80b6827bba280
KaeserOfHonour <kaeser418@gmail.com>
2023-07-04 12:29:18 +0300

    Added regex for folder color

--- src/controllers/folders/foldersVallidation.ts
+++ src/controllers/folders/foldersVallidation.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { z as zod } from "zod";
 
+const colorRegex = /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/g;
+
 const schemaFolder = zod.object({
     title: zod.string().nonempty({ message: "Enter the title" }).max(255, { message: "Title must be at maximum 255 characters long" }),
-    color: zod.string().optional(),
+    color: zod.string().regex(colorRegex, { message: "Enter a valid color" }).optional(),
 });
 
 export const validateFolder = async (req: Request, res: Response, next: NextFunction) => {
